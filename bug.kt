bff1bb18d40849a24d916ac02d576765ea4774e3
BugHunter-X <askzenai@gmail.com>
2025-01-25 16:48:07 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,20 @@
+```kotlin
+fun main() {
+    val list = mutableListOf<Int>(1, 2, 3, 4, 5)
+    list.removeIf { it > 2 }
+    println(list) // Output: [1, 2]
+
+    val set = mutableSetOf<Int>(1, 2, 3, 4, 5)
+    set.removeIf { it > 2 }
+    println(set) // Output: [1, 2] 
+
+    //The following will throw a ConcurrentModificationException
+    val list2 = mutableListOf<Int>(1, 2, 3, 4, 5)
+    for (i in list2) {
+        if (i > 2) {
+            list2.remove(i)
+        }
+    }
+    println(list2) 
+}
+```
